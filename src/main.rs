7635ce6038480a95113c8bc579a1a5ffcbe3c883
Randy Yeung <h0riz0n.yeung@gmail.com>
2020-04-20 23:48:50 -0500

    (try to) implement a function that spits out a hashset of paths(files)

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@
 
 // File or directory?
 // If file
-// print out file as designed
+//  print out file as designed
 // else
-// recursively get a list of all the files inside a directory
+//  recursively get a list of all the files inside a directory
 // for each file
-// print out file as designed
+//  print out file as designed
 
 // TODO: read keyboard q to quit instead of ctrl c
 //       more generic list of file types to read (rs, java, json, py)
@@ -20,6 +20,9 @@ use std::io::{self, Write};
 use std::path::{Path, PathBuf};
 use std::{thread, time};
 use structopt::StructOpt;
+use std::collections::HashSet;
+use glob::glob_with;
+use glob::MatchOptions;
 
 const PAUSE: [u16; 5] = [15, 25, 35, 35, 100];
 
@@ -34,8 +37,6 @@ fn main() {
 
     for path in paths {
         if path.is_file() {
-            // if input.is_file() {
-
             process_file(&path);
         } else if path.is_dir() {
             println!("{} is a directory", path.display());
@@ -48,10 +49,23 @@ fn main() {
                     Err(e) => println!("{:?}", e),
                 }
             }
+
+            get_file_list(&path);
+
         } else {
             println!("Can't determin what {} is", path.display());
         }
     }
+}
+
+fn get_file_list(directory: &Path) -> HashSet<&Path> {
+
+    let mut files: HashSet<&Path> = HashSet::new();
+
+    files.insert(directory);
+
+    return files;
+
 }
 
 fn process_file(input: &Path) {
